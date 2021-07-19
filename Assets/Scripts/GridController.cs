ebbce206a43556222f99cceade281a5595829e94
Jakub Bojarowski <jakubbojarowski82@gmail.com>
2021-07-19 21:19:07 +0200

    initial commit2

---
+++ Assets/Scripts/GridController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridController : MonoBehaviour
+{
+    [System.NonSerialized]
+    public Tile[,] TileGrid;
+    public Vector2Int GridSize;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
